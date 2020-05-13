362ed5ed563b52ef6b09f64109b61f13ef1ab061
shihuafan <shihuafan@gmail.com>
2020-05-14 03:46:25 +0800

    /

---
+++ app/src/main/java/com/example/flint/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.example.flint
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("Registered")
+open class BaseActivity : AppCompatActivity() {
+
+}
